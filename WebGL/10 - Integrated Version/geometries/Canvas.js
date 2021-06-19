592103dca7d1a4817d421392b908e2129493181e
Gauri04 <ranadegauri04@gmail.com>
2021-06-19 12:05:24 +0530

    GR geomrtry first scene

---
+++ WebGL/10 - Integrated Version/geometries/Canvas.js
@@ -0,0 +1,209 @@
+var canvas = null;
+var gl = null;
+var canvas_original_width;
+var canvas_original_height;
+var bFullscreen = false;
+var requestAnimationFrame = window.requestAnimationFrame || 
+                            window.webkitRequestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.oRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
+
+var cancelAnimationFrame = window.cancelAnimationFrame || 
+                        window.webkitCancelRequestAnimationFrame || window.webkitCancelAnimationFrame ||
+                        window.mozCancelRequestAnimationFrame || window.mozCancelAnimationFrame ||
+                        window.onCancelRequestAnimationFrame || window.onCancelAnimationFrame ||
+                        window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame;
+
+function main()
+{
+   
+    // get  canvas from DOM
+    canvas = document.getElementById("GR");
+    if(!canvas)
+        console.log("\n ontaining canvas failed");
+    else
+        console.log("\n ontaining canvas succeeded");
+
+    canvas_original_width = canvas.width;
+    canvas_original_height = canvas.height;
+
+    // "keydown" and "click" are in-built events. keyDown and mouseDown are user defined functions
+    // param false states that events are bubble events, not captured events 
+    window.addEventListener("keydown", keyDown, false);         // window is in-build variable, just like document. window is inherited from document, its a DOM object.
+    window.addEventListener("click", mouseDown, false);
+    window.addEventListener("resize", resize, false);
+
+    init();
+
+    resize();                                   
+    // till now in window, linux and android we did not have to call display() / draw() as it internally calls repaint. But here we have to call it as it doesnt do it 
+    // internally
+    // warm up call
+    draw();
+}
+
+
+
+
+function toggleFullscreen()
+{
+    var fullscreen_element = document.fullscreenElement || 
+                            document.webkitFullscreenElement ||         // apple - safari fullscreen
+                            document.mozFullScreenElement ||            // fullscreen for mozilla
+                            document.msFullscreenElement ||             // fullscreen for internet explorer / edge
+                            null;                                       // fullscreen for browsers other than the above three
+
+    if(fullscreen_element == null)
+    {
+         // set fullscreen
+        if(canvas.requestFullscreen)                                        // check whether function pointer is not null
+        {
+            canvas.requestFullscreen();
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen();
+        }
+        else if(canvas.mozRequestFullScreen)
+        {
+            canvas.mozRequestFullScreen();
+        }
+        else if(canvas.msRequestFullscreen)
+        {
+          canvas.msRequestFullscreen();
+        }
+        bFullscreen = true;   
+    }
+    else
+    {
+        if(document.exitFullscreen)
+        {
+            document.exitFullscreen();
+        }
+        else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen();
+        }
+        else if(document.mozCancelFullScreen)
+        {
+            document.mozCancelFullScreen();
+        }
+        else if(document.msExitFullscreen)
+        {
+            document.msExitFullscreen();
+        }
+        bFullscreen = false;
+    }
+}
+
+
+// because of type inference, we dont need to write data type of any variable
+function keyDown(event)
+{
+   switch(event.keyCode)
+   {
+       case 70 :
+            toggleFullscreen();
+            // as there is no repaint event in javascript
+            break;
+
+        case 88 :                   // x key
+            grfangleX = grfangleX + 1.0;
+            if(grfangleX >= 360.0)
+            {
+                grfangleX = 0.0;
+            }
+            break;
+
+        case 89 :                               // y key
+            grfangleY = grfangleY + 1.0;
+            if(grfangleY >= 360.0)
+            {
+                grfangleY = 0.0;
+            }
+            break;
+   }
+}
+
+function mouseDown()
+{
+    // code
+   
+}
+
+function init()
+{
+    // get drawing context from canvas
+    // to maintain consistency, we will get context in init() instead of in main()
+    gl = canvas.getContext("webgl2");
+
+    if(!gl)
+        console.log("webgl2 context failed");
+    else
+        console.log("webgl2 context succeeded");
+
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+
+   
+    // initialize stack matrix
+    //grstackMatrix[0] = mat4.create();
+    GRInit();
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    grgPerspectiveProjectionMatrix = mat4.create();
+    // blue color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+     
+}
+
+function resize()
+{
+    if(bFullscreen == true)
+    {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    else
+    {
+        canvas.width = canvas_original_width;
+        canvas.height = canvas_original_height;
+    }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    mat4.perspective(grgPerspectiveProjectionMatrix, 45.0, parseFloat(canvas.width) / parseFloat(canvas.height), 0.1, 100.0);
+
+}
+
+function draw()
+{
+    
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+
+    GRDisplay();
+    requestAnimationFrame(draw, canvas);
+}
+
+function update()
+{
+    
+}
+
+
+function deg2rad(degrees)
+{
+    var rad = degrees * Math.PI / 180.0;
+    return rad;
+}
+
+function uninitialize()
+{
+    GRUninitialize();
+
+}
+
+
+
+
